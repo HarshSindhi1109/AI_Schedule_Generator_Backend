f04fe8849e6cdd3c24285e1438b639c7acf372cf
Harsh.java <harshsindhi085@gmail.com>
2025-08-14 13:27:15 +0530

    Debug resolved for users

--- backend/app/models/users.py
+++ backend/app/models/users.py
@@ -11,4 +11,4 @@ class User(Base):
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     created_at = Column(TIMESTAMP(timezone=True), server_default=func.now())
 
-    timetables = relationship("Timetable", back_populates="semester")
+    timetables = relationship("Timetable", back_populates="user")
