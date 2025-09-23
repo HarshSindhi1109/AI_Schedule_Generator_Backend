cc09f941e0b3c4fc7a1cebb18d4004781d50891a
Harsh.java <harshsindhi085@gmail.com>
2025-09-23 17:03:54 +0530

    Modification to Accomodate Frontend

--- backend/app/schemas/users.py
+++ backend/app/schemas/users.py
@@ -1,10 +1,14 @@
 from pydantic import BaseModel
 from uuid import UUID
 from datetime import datetime
+from typing import Optional
+
+class UserCreate(BaseModel):
+    id: Optional[UUID] = None
 
 class UserBase(BaseModel):
     id: UUID
     created_at: datetime
 
     class Config:
-        orm_mode = True
+        from_attributes = True
