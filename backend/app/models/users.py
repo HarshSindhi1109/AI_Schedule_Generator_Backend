6671c5fd444b0285cbb78a7c3456425fd8b89794
Harsh.java <harshsindhi085@gmail.com>
2025-09-23 17:02:13 +0530

    Modification to Accomodate Frontend

--- backend/app/models/users.py
+++ backend/app/models/users.py
@@ -1,5 +1,5 @@
 import uuid
-from sqlalchemy import Column, TIMESTAMP
+from sqlalchemy import Column, String, Integer, TIMESTAMP
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
@@ -9,6 +9,8 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    role = Column(String, default='user')
+    token_version = Column(Integer, default=0)
     created_at = Column(TIMESTAMP(timezone=True), server_default=func.now())
 
     timetables = relationship("Timetable", back_populates="user")
